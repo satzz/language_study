6add6bec6ac7935406ab3551535695725c0d5883
satoda.akihiko <satzz@ip-10-156-87-83.ap-northeast-1.compute.internal>
2011-09-19 03:50:09 -0400

    pythag

--- erlang/lib_misc.erl
+++ erlang/lib_misc.erl
@@ -1,8 +1,20 @@
 -module(lib_misc).
 -export([for/3]).
 -export([qsort/1]).
+-export([pythag/1]).
 for(Max,Max,F)->[F(Max)];
 for(I,Max,F)->[F(I)|for(I+1,Max,F)].
 
 qsort([])->[];
 qsort([Pivot|T])->qsort([X||X<-T, X<Pivot]) ++ [Pivot] ++ qsort([X||X<-T, X>=Pivot]).
+
+pythag(N)->
+[{A,B,C}||
+    A<-lists:seq(1,N),
+    B<-lists:seq(1,N),
+    C<-lists:seq(1,N),
+    A+B+C =< N,
+    A*A+B*B =:= C*C
+].
+
+    
