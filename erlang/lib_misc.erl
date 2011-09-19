eedcdcf7e8c126e5e9b54cbdd63dee887905054a
satoda.akihiko <satzz@ip-10-156-87-83.ap-northeast-1.compute.internal>
2011-09-19 09:30:52 -0400

    odds and evens

--- erlang/lib_misc.erl
+++ erlang/lib_misc.erl
@@ -3,6 +3,8 @@
 -export([qsort/1]).
 -export([pythag/1]).
 -export([perms/1]).
+-export([odds_and_evens/1]).
+-export([odds_and_evens_acc/1]).
 for(Max,Max,F)->[F(Max)];
 for(I,Max,F)->[F(I)|for(I+1,Max,F)].
 
@@ -20,4 +22,18 @@ pythag(N)->
 
 perms([])->[[]];
 perms(K)->[[H|T] || H<-K, T<-perms(K--[H])].
-    
+
+odds_and_evens(L)->
+    Odds = [X|| X<-L, (X rem 2) =:=1],
+    Evens = [X || X<-L, (X rem 2) =:=0],
+    {Odds, Evens}.
+
+odds_and_evens_acc(L)->
+    odds_and_evens_acc(L,[],[]).
+odds_and_evens_acc([H|T], Odds, Evens)->
+    case(H rem 2) of
+        1 -> odds_and_evens_acc(T, [H|Odds], Evens);
+        0 -> odds_and_evens_acc(T, Odds, [H|Evens])
+    end;
+odds_and_evens_acc([], Odds, Evens)->
+    {Odds, Evens}.
