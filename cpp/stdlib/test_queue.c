935eac86cbcc160af48c66ec8a0c1e37b9b82c1c
satoda.akihiko <rackhuber@satzz.rackbox.net>
2012-06-03 07:48:09 +0000

    stack and queue

---
+++ cpp/stdlib/test_queue.c
@@ -0,0 +1,18 @@
+#include <queue>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    queue<int> que;
+    que.push(1);
+    que.push(2);
+    que.push(3);
+    printf("%d\n", que.front());
+    que.pop();
+    printf("%d\n", que.front());
+    que.pop();
+    printf("%d\n", que.front());
+    que.pop();
+    return 0;
+}
